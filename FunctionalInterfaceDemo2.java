3711e796766318b5bc26616a7a18c25519218a6f
sriganeshmenni <mennisri2005@gmail.com>
2024-10-16 14:47:25 +0530

    commit

--- FunctionalInterfaceDemo2.java
+++ FunctionalInterfaceDemo2.java
@@ -1,10 +1,10 @@
-interface Test
+interface Test1
 {
     void print(String name);
 }
 public class FunctionalInterfaceDemo2 {
     public static void main(String[] args) {
-        Test t = (name)->{System.out.println("Hello! "+name);};
+        Test1 t = (name)->{System.out.println("Hello! "+name);};
         t.print("Aditya");
     }
 }
